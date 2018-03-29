b591063ce726ef53ddb9ad060daae14d7ad23d0c
Conor Burrows <conor@ironicrobot.com>
2018-03-29 14:59:32 -0500

    Added Projectile System
    Projectile system implemented and it works in VR

---
+++ Assets/Scripts/ControllerManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerManager : MonoBehaviour
+{
+    public bool triggerButtonDown = false;
+    public Rigidbody spellPrefab;
+    public Transform firePosition;
+
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    private SteamVR_Controller.Device controller
+    {
+
+        get
+        {
+            return SteamVR_Controller.Input((int)trackedObj.index);
+        }
+    }
+
+    private SteamVR_TrackedObject trackedObj;
+
+    void Start()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void Update()
+    {
+
+        if (controller == null)
+        {
+            Debug.Log("Controller not initialized");
+
+            return;
+        }
+
+        triggerButtonDown = controller.GetPressDown(triggerButton);
+
+        if (triggerButtonDown)
+        {
+            Debug.Log("Fire");
+            Rigidbody spellInstance;
+            spellInstance = Instantiate(spellPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
+            spellInstance.AddForce(firePosition.forward * 2000);
+        }
+    }
+}
+
