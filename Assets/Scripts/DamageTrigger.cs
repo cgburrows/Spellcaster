873afc45158d9705df042a573175bdc31f26773e
Conor Burrows <conor@ironicrobot.com>
2018-04-02 13:13:59 -0500

    Updated camera rig and some scripts
    Added framework for damage triggers and made some camera rig QOL changes.

---
+++ Assets/Scripts/DamageTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTrigger : MonoBehaviour {
+
+    public int despawnTime = 5;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void OnCollisionEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Target"))
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
