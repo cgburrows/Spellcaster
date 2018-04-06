53c59b6965f1d2c07d23792f8b797552513a5e6e
Jerome-Richards <jeromerichards1996@gmail.com>
2018-04-06 02:08:34 -0500

    Added Selection wheel
    and a script that should activate it when the trackpad is touched, havent tested it though

---
+++ Assets/Scripts/SelectionWheel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionWheel : MonoBehaviour {
+    private SteamVR_TrackedController rightController;
+    private bool activity;
+    public GameObject SelectionWheel1;
+	// Use this for initialization
+	void Start () {
+        activity = false;
+	}
+
+    private void OnEnable()
+    {
+        rightController = GetComponent<SteamVR_TrackedController>();
+        
+    }
+    // Update is called once per frame
+    void Update () {
+        SelectionWheel1.SetActive(activity);
+        if (rightController.padTouched)
+        {
+            activity = true;
+        }
+        else
+            activity = false;
+	}
+    
+}
